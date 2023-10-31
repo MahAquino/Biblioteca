1473ad49695768b6d1a319e3d59610b5bfc8c758
Márcia Aquino <marciaatule@gmail.com>
2023-10-31 16:52:32 -0300

    criação da tabela autores

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -9,7 +9,14 @@ autor_id  int NOT NULL,
 ano_publicacao int NOT NULL
 );
 
+-- criar tabela autores
+CREATE TABLE Autores (
+ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome VARCHAR (200) NOT NULL,
+nacionalidade VARCHAR (50)  NOT NULL
+);
 
+-- criar tabela usuarios 
 
 
 

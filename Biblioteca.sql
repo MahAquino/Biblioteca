b461fcdb756f4ccb82d2ea1de2c71b7795c2c799
Márcia Aquino <marciaatule@gmail.com>
2023-10-31 16:51:29 -0300

    criação da tabela livros

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -1,6 +1,14 @@
 -- criar banco de dados chamado Biblioteca
 CREATE DATABASE Biblioteca;
 
+-- criar tabela livros
+CREATE TABLE Livros (
+ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+titulo VARCHAR (200) NOT NULL,
+autor_id  int NOT NULL,
+ano_publicacao int NOT NULL
+);
+
 
 
 
